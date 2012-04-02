c78077c962566015bb12f72e255beca36f13cb4f
Rudy Lippert <rudyl313@gmail.com>
2012-04-01 18:48:37 -0700

    Created unit tests for the table object using a mock token

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,42 @@ require 'factual'
 RSpec.configure do |c|
     c.mock_with :rspec
 end
+
+class MockAccessToken
+  class Response
+    def body
+      { "status" => "ok", "response" => response }.to_json
+    end
+
+    def response
+      {
+        "data" => [
+          { :key => "value1" },
+          { :key => "value2" },
+          { :key => "value3" }
+        ]
+      }
+    end
+  end
+
+  attr_reader :last_url
+
+  def initialize
+    @last_url = nil
+  end
+
+  def get(url, headers)
+    @last_url = url
+    Response.new
+  end
+end
+
+module TestHelpers
+  def get_token
+    MockAccessToken.new
+  end
+
+  def get_api(token)
+    Factual::API.new(token)
+  end
+end
--- spec/unit/table_spec.rb
+++ spec/unit/table_spec.rb
@@ -1,5 +1,115 @@
 require 'spec_helper'
 
 describe Factual::Query::Table do
-  pending "should have tests"
+  include TestHelpers
+
+  before(:each) do
+    @token = get_token
+    @api = get_api(@token)
+    @table_name = "places"
+    @table = Factual::Query::Table.new(@api, "t/#{@table_name}")
+    @base = "http://api.v3.factual.com/t/places?"
+  end
+
+  it "should be able to use filters" do
+    @table.filters("country" => "US").rows
+    expected_url = @base + "filters={\"country\":\"US\"}"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to search" do
+    @table.search("suchi", "sashimi").rows
+    expected_url = @base + "q=suchi,sashimi"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to use a geo parameter" do
+    @table.geo("$circle" => {"$center" => [34.06021, -118.41828],
+               "$meters" => 5000}).rows
+    expected_url = @base + "geo={\"$circle\":{\"$center\":[34.06021,-118.41828],\"$meters\":5000}}"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to sort in ascending order" do
+    @table.sort("name").rows
+    expected_url = @base + "sort=name"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to sort in descending order" do
+    @table.sort_desc("name").rows
+    expected_url = @base + "sort=name:desc"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to select specific columns" do
+    @table.select(:name, :website).rows
+    expected_url = @base + "select=name,website"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to limit the number of returned rows" do
+    @table.limit(5).rows
+    expected_url = @base + "limit=5"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to set an offset for a query" do
+    @table.offset(5).rows
+    expected_url = @base + "offset=5"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to set whether the count is included" do
+    @table.include_count(true).rows
+    expected_url = @base + "include_count=true"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to set the page" do
+    @table.page(5).rows
+    expected_url = @base + "limit=20&offset=80"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to get the total count" do
+    @table.total_count
+    expected_url = @base + "include_count=true&limit=1"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to get the schema" do
+    @table.schema
+    expected_url = "http://api.v3.factual.com/t/places/schema?"
+    CGI::unescape(@token.last_url).should == expected_url
+  end
+
+  it "should be able to fetch the action" do
+    @table.action.should == :read
+  end
+
+  it "should be able to fetch the path" do
+    @table.path.should == "t/places"
+  end
+
+  it "should be able to fetch the params" do
+    expected_params = {:filters => {"country"=>"US"}}
+    @table.filters("country" => "US").params.should == expected_params
+  end
+
+  it "should be able to fetch the rows" do
+    @table.rows.map { |r| r["key"] }.should == ["value1", "value2", "value3"]
+  end
+
+  it "should be able to get the first row" do
+    @table.first["key"].should == "value1"
+  end
+
+  it "should be able to get the last row" do
+    @table.last["key"].should == "value3"
+  end
+
+  it "should be able to get a value at a specific index" do
+    @table[1]["key"].should == "value2"
+  end
 end
