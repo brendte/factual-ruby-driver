f2d9fbcaca12043af9af1f7df438ba57f04038f8
Rudy Lippert <rudyl313@gmail.com>
2012-04-15 15:12:37 -0700

    Finished FLAG testing, integration testing requires update to API

--- spec/unit/flag_spec.rb
+++ spec/unit/flag_spec.rb
@@ -14,7 +14,7 @@ describe Factual::Flag do
     @flag = Factual::Flag.new(@api, @basic_params)
   end
 
-  it "should be able to use the minimal params (table, factual_id, problem, user)" do
+  it "should be able to write a basic flag" do
     @flag.write
     @token.last_url.should == "http://api.v3.factual.com/t/global/id123/flag"
     @token.last_body.should == "problem=duplicate&user=user123"
@@ -48,13 +48,23 @@ describe Factual::Flag do
     @token.last_body.should == "problem=duplicate&user=user123&comment=This+is+my+comment"
   end
 
-  it "should be able to set the debug flag"
+  it "should be able to set the debug flag" do
+    @flag.debug(true).write
+    @token.last_url.should == "http://api.v3.factual.com/t/global/id123/flag"
+    @token.last_body.should == "problem=duplicate&user=user123&debug=true"
+  end
 
-  it "should be able to set a reference"
+  it "should be able to set a reference" do
+    @flag.reference("http://www.google.com").write
+    @token.last_url.should == "http://api.v3.factual.com/t/global/id123/flag"
+    @token.last_body.should == "problem=duplicate&user=user123&reference=http%3A%2F%2Fwww.google.com"
+  end
 
-  it "should be able to return a path"
+  it "should be able to return a path" do
+    @flag.path.should == "/t/global/id123/flag"
+  end
 
-  it "should be able to return a body"
-
-  it "should be able to write the flag"
+  it "should be able to return a body" do
+    @flag.body.should == "problem=duplicate&user=user123"
+  end
 end
